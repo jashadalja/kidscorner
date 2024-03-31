b5a1bd6e0cf0bb790be706aa16c10c575d007f00
jashadalja <143736203+jashadalja@users.noreply.github.com>
2024-03-31 14:44:58 +0530

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,98 @@
+function sendMailAndRedirect() {
+    emailjs.send("service_2agyi6o", "template_m7whu4p", {
+        subject: " 📢 Notification: Your Child Is Accessing Kids Corner Website for Learning 📚",
+        message1: "We hope you know about that. 🌟",
+        motive: "We wanted to inform you that your child is --> doing art <--. 🖥️📖",
+        regards: "Best Regards, Kids Corner",
+    }).then(function (res) {
+        // Redirect to the new HTML document after the email is sent
+        window.location.href = 'drawing/drawing.html';
+    }).catch(function (error) {
+        console.error('Email sending failed:', error);
+        // Handle the error as needed
+    });
+}
+function sendMailAndRedirectGame1() {
+    emailjs.send("service_2agyi6o", "template_m7whu4p", {
+        subject: " 📢 Notification: Your Child Is Accessing Kids Corner Website for Learning 📚",
+        message1: "We hope you know about that. 🌟",
+        motive: "We wanted to inform you that your child is playing  --> Addition Game <-- on Kids Corner. 🖥️📖",
+        regards: "Best Regards, Kids Corner",
+    }).then(function (res) {
+        // Redirect to the new HTML document after the email is sent
+        window.location.href = 'https://www.starfall.com/h/addsub/addsub-ladder/';
+    }).catch(function (error) {
+        console.error('Email sending failed:', error);
+        // Handle the error as needed
+    });
+}
+function sendMailAndRedirectGame2() {
+    emailjs.send("service_2agyi6o", "template_m7whu4p", {
+        subject: " 📢 Notification: Your Child Is Accessing Kids Corner Website for Learning 📚",
+        message1: "We hope you know about that. 🌟",
+        motive: "We wanted to inform you that your child is playing --> Noun Game <-- on Kids Corner. 🖥️📖",
+        regards: "Best Regards, Kids Corner",
+    }).then(function (res) {
+        // Redirect to the new HTML document after the email is sent
+        window.location.href = 'https://www.starfall.com/h/ela/nounsort/?sn=ela1';
+    }).catch(function (error) {
+        console.error('Email sending failed:', error);
+        // Handle the error as needed
+    });
+}
+function sendMailAndRedirectGame3() {
+    emailjs.send("service_2agyi6o", "template_m7whu4p", {
+        subject: " 📢 Notification: Your Child Is Accessing Kids Corner Website for Learning 📚",
+        message1: "We hope you know about that. 🌟",
+        motive: "We wanted to inform you that your child is playing --> Clock Game <-- on Kids Corner. 🖥️📖",
+        regards: "Best Regards, Kids Corner",
+    }).then(function (res) {
+        // Redirect to the new HTML document after the email is sent
+        window.location.href = 'https://www.starfall.com/h/geometry/match-clocks/?mg=k';
+    }).catch(function (error) {
+        console.error('Email sending failed:', error);
+        // Handle the error as needed
+    });
+}
+function sendMailAndRedirectGame4() {
+    emailjs.send("service_2agyi6o", "template_m7whu4p", {
+        subject: " 📢 Notification: Your Child Is Accessing Kids Corner Website for Learning 📚",
+        message1: "We hope you know about that. 🌟",
+        motive: "We wanted to inform you that your child is playing --> puzzle Game <-- on Kids Corner. 🖥️📖",
+        regards: "Best Regards, Kids Corner",
+    }).then(function (res) {
+        // Redirect to the new HTML document after the email is sent
+        window.location.href = 'https://www.starfall.com/h/geometry/puzzles-easy/?mg=k';
+    }).catch(function (error) {
+        console.error('Email sending failed:', error);
+        // Handle the error as needed
+    });
+}
+function sendMailAndRedirectGame5() {
+    emailjs.send("service_2agyi6o", "template_m7whu4p", {
+        subject: " 📢 Notification: Your Child Is Accessing Kids Corner Website for Learning 📚",
+        message1: "We hope you know about that. 🌟",
+        motive: "We wanted to inform you that your child is playing --> Monkey Mash Game <-- on Kids Corner. 🖥️📖",
+        regards: "Best Regards, Kids Corner",
+    }).then(function (res) {
+        // Redirect to the new HTML document after the email is sent
+        window.location.href = 'https://www.starfall.com/h/addsub/monkey-mash/?sn=math2';
+    }).catch(function (error) {
+        console.error('Email sending failed:', error);
+        // Handle the error as needed
+    });
+}
+function sendMailAndRedirectGame6() {
+    emailjs.send("service_2agyi6o", "template_m7whu4p", {
+        subject: " 📢 Notification: Your Child Is Accessing Kids Corner Website for Learning 📚",
+        message1: "We hope you know about that. 🌟",
+        motive: "We wanted to inform you that your child is playing --> Card Game <-- on Kids Corner. 🖥️📖",
+        regards: "Best Regards, Kids Corner",
+    }).then(function (res) {
+        // Redirect to the new HTML document after the email is sent
+        window.location.href = 'https://www.starfall.com/h/multdiv/mult03xb/?sn=math2';
+    }).catch(function (error) {
+        console.error('Email sending failed:', error);
+        // Handle the error as needed
+    });
+}
